7ce9e2ca7827f5493b4ac3c312162f9443667a99
kmorlock <killianmorlock@gmail.com>
2018-03-21 16:30:43 +0100

    [Sprint 1/Sprint 2] Couche services 100%

--- src/main/java/com/projet/service/ClientController.java
+++ src/main/java/com/projet/service/ClientController.java
@@ -3,7 +3,11 @@ package com.projet.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projet.dao.ClientDao;
@@ -15,9 +19,32 @@ public class ClientController {
     @Autowired
     ClientDao clientDAO;
 
+    @RequestMapping(value = "/client", method = RequestMethod.POST)
+    public int createClient(@RequestBody Client client) {
+    	Client createdClient = clientDAO.create(client);
+        return createdClient.getId();
+    }
+    
     @RequestMapping("/client")
-    public List<Client> client() {
+    public List<Client> getAll() {
     	List<Client> lesClients = clientDAO.findAll();
         return lesClients;
     }
+
+    @RequestMapping("/client/{id}")
+    public Client getClientById(@PathVariable("id") int id) {
+    	Client client = clientDAO.findClientById(id);
+        return client;
+    }
+    
+    @RequestMapping(value = "/client", method = RequestMethod.PUT)
+    public void updateClient(@RequestBody Client client) {
+    	clientDAO.updateClient(client);
+    }
+    
+    @RequestMapping(value = "/client", method = RequestMethod.DELETE)
+    public void deleteClient(@RequestParam("id") int id) {
+    	clientDAO.deleteClient(id);
+    }
+    
 }
--- src/main/java/com/projet/service/ProduitController.java
+++ src/main/java/com/projet/service/ProduitController.java
@@ -3,10 +3,15 @@ package com.projet.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projet.dao.ProduitDao;
+import com.projet.model.Produit;
+import com.projet.model.Produit;
 import com.projet.model.Produit;
 
 @RestController
@@ -15,9 +20,31 @@ public class ProduitController {
     @Autowired
     ProduitDao produitDAO;
 
+    @RequestMapping(value = "/produit", method = RequestMethod.POST)
+    public String createProduit(@RequestBody Produit produit) {
+    	Produit createdProduit = produitDAO.create(produit);
+        return createdProduit.getId();
+    }
+
     @RequestMapping("/produit")
     public List<Produit> produit() {
     	List<Produit> lesProduits = produitDAO.findAll();
         return lesProduits;
+    }
+
+    @RequestMapping("/produit/{id}")
+    public Produit getProduitById(@PathVariable("id") String id) {
+    	Produit produit = produitDAO.findProduitById(id);
+        return produit;
+    }
+    
+    @RequestMapping(value = "/produit", method = RequestMethod.PUT)
+    public void updateProduit(@RequestBody Produit produit) {
+    	produitDAO.updateProduit(produit);
+    }
+    
+    @RequestMapping(value = "/produit/{id}", method = RequestMethod.DELETE)
+    public void deleteProduit(@PathVariable("id") String id) {
+    	produitDAO.deleteProduit(id);
     }
 }
